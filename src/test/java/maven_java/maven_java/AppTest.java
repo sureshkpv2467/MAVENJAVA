01e3a498ecba4d893ca083e5a0188c6fde7404d3
sureshkpv2467 <sureshkpv21@gmail.com>
2023-12-08 10:14:11 +0530

    first comit

---
+++ src/test/java/maven_java/maven_java/AppTest.java
@@ -0,0 +1,20 @@
+package maven_java.maven_java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
